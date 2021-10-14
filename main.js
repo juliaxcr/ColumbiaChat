02bbe8948fdf667eb610c69720af6fa74b37878e
Julia Crooijmans <juliacrooijmans@gmail.com>
2021-10-14 14:16:19 -0400

    launched window

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadFile('index.html')
+}
+app.whenReady().then(createWindow)
